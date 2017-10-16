eeb260370396dada2432a57d64e03aa3920a34f9
CCliff <Clcliff30@gmail.com>
2017-10-16 15:15:55 -0400

    test

--- app/server.js
+++ app/server.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const express = require('express');
-
+const SlackBot = require('./SlackBot.js');
 // Constants
 const PORT = process.env.PORT || 8080;
 
 // App
 const app = express();
+const slackBot = new SlackBot();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+});
+
+app.get('/slack', (req, res) => {
+  console.log('Message from Slack: ',req);
 });
 
 app.listen(PORT, function(){
---
+++ app/slackBot.js
@@ -0,0 +1,48 @@
+const RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+
+class SlackBot{
+  constructor() {
+    this.bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-257203437157-T0QpzJifeBX09vs5kDwkVmBj';
+    this.channel = '';
+
+    this.init();
+  }
+
+  init() {
+    this.rtm = new RtmClient(bot_token);
+    this.setupEventHandlers();
+
+    this.rtm.start();
+  }
+
+  setupEventHandlers(){
+    this.rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, this.authenticated);
+    this.rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, this.connected);
+    this.rtm.on(RTM_EVENTS.MESSAGE, this.receiveMessage);
+  }
+
+  authenticated(rtmStartData) {
+    for (const c of rtmStartData.channels) {
+      if (c.is_member && c.name ==='general') { this.channel = c.id }
+    }
+
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+  }
+
+  connected() {
+    this.sendMessage("Hello, world!");
+  }
+
+  receiveMessage(message) {
+    console.log('Message: ', message);
+  }
+
+  sendMessage(message) {
+    rtm.sendMessage(message, this.channel);
+  }
+}
+
+exports.SlackBot = SlackBot;
