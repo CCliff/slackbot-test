44ae71cfc752a1ec4d2d2ec250e7bc5f7bf66c67
CCliff <Clcliff30@gmail.com>
2017-10-16 14:15:06 -0400

    Test

--- app/server.js
+++ app/server.js
@@ -4,14 +4,14 @@ const express = require('express');
 var Slack = require('@slack/client');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world2\n');
+  res.send('Hello world\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, function(){
+  // console.log(`Running on http://${HOST}:${PORT}`);
+});
