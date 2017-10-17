69c8986789a5eceeae74f702108253f69274180e
CCliff <Clcliff30@gmail.com>
2017-10-17 12:49:43 -0400

    Adding body-parser

--- app/server.js
+++ app/server.js
@@ -3,23 +3,32 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-
-const express = require('express');
-const SlackBotController = require('./controllers/slackBotController.js').SlackBotController;
-// Constants
+//   Constants  //
 const PORT = process.env.PORT || 8080;
+//  /Constants  //
 
-// App
+const express    = require('express');
+const bodyParser = require("body-parser");
+
+const SlackBotController = require('./controllers/slackBotController.js').SlackBotController;
+
+//   App
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const slackBot = new SlackBotController();
+
+//   Routes  //
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
-app.get('/slack', (req, res) => {
-  // console.log(req);
+app.post('/slack', (req, res) => {
+  console.log(req.body);
   res.sendStatus(200);
 });
+//  /Routes  //
 
 app.listen(PORT, function(){
   console.log(`Running on port: ${PORT}`);
